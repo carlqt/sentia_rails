af98df34e5db69ed99224107c24b671d71a47744
Carl Tablante <carlwilliam.tablante@gmail.com>
2023-07-27 15:29:48 +1000

    Building Importer service

--- app/models/importer.rb
+++ app/models/importer.rb
@@ -1,9 +1,43 @@
 class Importer
   def initialize(file)
     @file = file.open
+    @data_list = []
   end
 
   def run
+    @csv.each do |row|
+      next if row[:Affiliations].blank?
+
+      person = build_person(row)
+      locations = build_location(row)
+      affiliations = build_affiliation(row)
+    end
+  end
+
+  def build_person(row)
+    Person.new.tap do |person|
+      person.name: row[:Name]
+      person.species = row[:Species]
+      person.gender = row[:Gender]
+      person.weapon = row[:Weapon]
+      person.vehicle = row[:Vehicle]
+    end
+  end
+
+  def build_location(row)
+    locations_row = row[:Location].split(',')
+
+    locations_row.map do |loc_name|
+      Location.new(name: loc_name)
+    end
+  end
+
+  def build_affiliation(row)
+    affiliations_row = row[:Location].split(',')
+
+    affiliations_row.map do |aff_name|
+      Affiliation.new(name: aff_name)
+    end
   end
 
   def csv
