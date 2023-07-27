5bb92fd0eab815b1398db281e27b8b09b22026f8
Carl Tablante <carlwilliam.tablante@gmail.com>
2023-07-27 13:56:29 +1000

    Affiliation MOdel

---
+++ db/migrate/20230727035614_create_affiliations.rb
@@ -0,0 +1,10 @@
+class CreateAffiliations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :affiliations do |t|
+      t.string :name
+      t.references :person, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_27_035102) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_27_035614) do
+  create_table "affiliations", force: :cascade do |t|
+    t.string "name"
+    t.integer "person_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["person_id"], name: "index_affiliations_on_person_id"
+  end
+
   create_table "locations", force: :cascade do |t|
     t.string "name"
     t.integer "person_id", null: false
@@ -30,5 +38,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_035102) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "affiliations", "people"
   add_foreign_key "locations", "people"
 end
